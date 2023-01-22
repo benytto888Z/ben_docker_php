62dcf06fff1800b41d3834383038e8b7138d60b2
benytto888Z <benamos111@gmail.com>
2023-01-22 17:28:56 +0100

    add multi-stage-build

--- app/public/index.php
+++ app/public/index.php
@@ -4,6 +4,8 @@ use App\Repository\TranslationRepository;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+xdebug_info();
+
 $client = \Symfony\Component\Cache\Adapter\RedisAdapter::createConnection(
     "redis://{$_ENV['REDIS_HOST']}:{$_ENV['REDIS_PORT']}"
 );
